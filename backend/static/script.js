1adf40d543b36bd68ce7cc5d964f242c7496c484
Vishal Baibhav Panda <officialvishalbaibhav@gmail.com>
2026-03-17 21:50:38 +0530

    Update fetch URL to relative path

--- backend/static/script.js
+++ backend/static/script.js
@@ -81,7 +81,7 @@ async function sendMessage() {
     chatBox.appendChild(typing);
 
     try {
-        const res = await fetch("http://127.0.0.1:5000/chat", {
+        const res = await fetch("/chat", {
             method: "POST",
             headers: {"Content-Type": "application/json"},
             body: JSON.stringify({ message })
